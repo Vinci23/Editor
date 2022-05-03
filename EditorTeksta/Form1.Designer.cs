d8c4c0521346e401e0c8c07279373649f404430f
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-05-03 13:13:48 +0200

    Add files via upload

---
+++ EditorTeksta/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace EditorTeksta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnBold = new System.Windows.Forms.Button();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.btnUnder = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(369, 366);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnBold
+            // 
+            this.btnBold.Location = new System.Drawing.Point(411, 42);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(64, 58);
+            this.btnBold.TabIndex = 1;
+            this.btnBold.Text = "Bold";
+            this.btnBold.UseVisualStyleBackColor = true;
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.Location = new System.Drawing.Point(411, 122);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(64, 58);
+            this.btnItalic.TabIndex = 2;
+            this.btnItalic.Text = "Italic";
+            this.btnItalic.UseVisualStyleBackColor = true;
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click);
+            // 
+            // btnUnder
+            // 
+            this.btnUnder.Location = new System.Drawing.Point(411, 202);
+            this.btnUnder.Name = "btnUnder";
+            this.btnUnder.Size = new System.Drawing.Size(62, 58);
+            this.btnUnder.TabIndex = 3;
+            this.btnUnder.Text = "Underline";
+            this.btnUnder.UseVisualStyleBackColor = true;
+            this.btnUnder.Click += new System.EventHandler(this.btnUnder_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(612, 295);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(161, 72);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "OPEN";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(411, 295);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(161, 68);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnUnder);
+            this.Controls.Add(this.btnItalic);
+            this.Controls.Add(this.btnBold);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnBold;
+        private System.Windows.Forms.Button btnItalic;
+        private System.Windows.Forms.Button btnUnder;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ EditorTeksta/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EditorTeksta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            /*if (!Directory.Exists(@"c:\Editor"))
+            {
+                Directory.CreateDirectory(@"c:\Editor");
+
+            }
+            */
+
+            string path = @"c:\Editor\editor.txt";
+            if (File.Exists(path)) 
+            { 
+                StreamWriter sw = new StreamWriter(path);
+                sw.WriteLine(richTextBox1.Text);
+                sw.Close();
+            }
+            else
+            {
+
+            }
+            
+            
+           
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            
+            string path = @"c:\Editor\editor.txt";
+            StreamReader stream = new StreamReader(path);
+            string file = stream.ReadToEnd(); 
+            richTextBox1.Text = file.ToString();
+            stream.Close();
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Bold);
+            if (btnItalic !=null)
+            {
+                richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Bold | FontStyle.Italic);
+            }
+            else if(btnUnder !=null)
+            {
+                richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Bold | FontStyle.Underline);
+            }
+            else
+            {
+                richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Bold | FontStyle.Underline | FontStyle.Italic);
+            }
+           
+
+        }
+
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Italic);
+        }
+
+        private void btnUnder_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Underline);
+        }
+    }
+}
